37af8c8b16fdf90ca50e19141e8ad7ac940018e8
Ingyu Woo <iglenwoo@gmail.com>
2019-04-16 13:29:01 -0700

    parse req body, add todos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json()); // for parsing application/json
+// NOTE: 'parsing application/x-www-form-urlencoded' is necessary.
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => res.send('Not supported'));
 
 let message = {
     text: 'Text message',
@@ -11,9 +17,37 @@ let message = {
     ]
 };
 
+const parseQueryString = (query) => {
+    if (!query) return;
+
+    const parts = query.split(' ');
+    const majorClassNum = parts[0];// todo: split to major and classNum
+
+    let major, classNum;
+
+    return {
+        major, classNum
+    }
+};
+
+const fetchOSUClass = (major, classNum) => {
+    // https://classes.oregonstate.edu/api/?page=fose&route=search&alias=cs553
+    request.get(`https://classes.oregonstate.edu/api/?page=fose&route=search&subject=${major}${classNum}`);
+};
+
 app.post('/class', (req, res) => {
+    console.log(req.body); // todo: pass req.body.text to parser
+
+    // TODO: get class info
+
+    // TODO: get professor rating
+
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(message));
+});
+
+app.post('/prof', (req, res) => {
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
