9631764cd65e82b9fe36fed6745105203f2095b8
Ingyu Woo <iglenwoo@gmail.com>
2019-04-18 10:52:15 -0700

    fetch class details

--- src/index.js
+++ src/index.js
@@ -4,14 +4,32 @@ const request = require('request');
 const app = express();
 const port = 4000;
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json());
 // NOTE: 'parsing application/x-www-form-urlencoded' is necessary.
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => res.send('Not supported'));
 
-const fetchOSUClass = (classCode) => {
+const fetchClassDetails = (code, crn, srcdb) => {
+    const headers = {
+        contentType: 'application/json'
+    };
+    const url = 'https://classes.oregonstate.edu/api/?page=fose&route=details';
+    const body = `{"group":"code:${code}","key":"crn:${crn}","srcdb":"${srcdb}","matched":"crn:${crn}"}`;
 
+    request.post({
+        headers,
+        url,
+        body
+    }, (err, response, body) => {
+        console.log('error:', err); // Print the error if one occurred
+        console.log('httpResponse:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        // todo: get prof
+    });
+};
+
+const searchClass = (classCode) => {
     const headers = {
         contentType: 'application/json'
     };
@@ -26,6 +44,10 @@ const fetchOSUClass = (classCode) => {
         console.log('error:', err); // Print the error if one occurred
         console.log('httpResponse:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
+        const json = JSON.parse(body);
+        Object.values(json.results).forEach((result) => {
+            fetchClassDetails(result.code, result.crn, result.srcdb);
+        });
     });
 };
 
@@ -38,7 +60,7 @@ let message = {
 
 app.post('/class', (req, res) => {
     // TODO: get class info
-    fetchOSUClass(req.body.text);
+    searchClass(req.body.text);
 
     // TODO: get professor rating
 
