7f066151421cca0bca9099ff06794d91f02e61e2
Ingyu Woo <iglenwoo@gmail.com>
2019-04-30 11:02:33 -0700

    refactor codes to use try catch

--- src/routes/class/index.js
+++ src/routes/class/index.js
@@ -1,27 +1,65 @@
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
 
-const fetchProf = async (url) => {
-  if (!url) return
+const searchOsuClass = async classCode => {
+  const url = 'https://classes.oregonstate.edu/api/?page=fose&route=search'
+  const body = `{"other":{"srcdb":"999999"},"criteria":[{"field":"alias","value":"${classCode}"}]}`
 
-  const response = await fetch(url)
-  const body = await response.text()
-
-  const $ = cheerio.load(body)
-  const html = $('div[class="breakdown-wrapper"]')
-  const quality = html.find('.quality-header').find('.grade').text().trim()
-  const takeAgain = html.find('.takeAgain').find('.grade').text().trim()
-  const difficulty = html.find('.difficulty').find('.grade').text().trim()
-
-  return {
-    quality,
-    takeAgain,
-    difficulty,
+  const response = await fetch(url, {
+    method: 'POST',
+    body,
+  })
+  const json = await response.json()
+  let result
+  if (json && json.results) {
+    if (json.results.length === 0) {
+      throw Error(`No class with code(${classCode})`)
+    }
+    if (json.results.length === 1) {
+      result = json.results[0]
+    } else {
+      // todo: multiple classes ?
+      throw Error(`Multiple classses found with code(${classCode})`)
+    }
+  } else {
+    throw Error(`No class with code(${classCode})`)
   }
+
+  return result
+}
+
+const validateOsuClass = (osuClass) => {
+  if (!osuClass) {
+    throw Error('Failed to get')
+  }
+  if (!osuClass.code) {
+    throw Error('Failed to get class code')
+  }
+  if (!osuClass.crn) {
+    throw Error('Failed to get crn')
+  }
+  if (!osuClass.srcdb) {
+    throw Error('Failed to get srcdb')
+  }
+
+  return osuClass
+}
+
+const getInstructor = async (code, crn, srcdb) => {
+  const url = 'https://classes.oregonstate.edu/api/?page=fose&route=details'
+  const body = `{"group":"code:${code}","key":"crn:${crn}","srcdb":"${srcdb}","matched":"crn:${crn}"}`
+
+  const response = await fetch(url, { method: 'POST', body })
+  const data = await response.text()
+  const json = JSON.parse(data)
+  const $ = cheerio.load(json.instructordetail_html)
+  return $('.instructor-detail').text()
 }
 
 const searchProf = async (prof) => {
-  if (!prof) return
+  if (!prof) {
+    throw Error('Failed to get professor name')
+  }
 
   const name = prof.replace(' ', '+')
 
@@ -48,92 +86,52 @@ const searchProf = async (prof) => {
   return profLink
 }
 
-const getInstructor = async (code, crn, srcdb) => {
-  if (!code || !crn || !srcdb) return
-
-  const url = 'https://classes.oregonstate.edu/api/?page=fose&route=details'
-  const body = `{"group":"code:${code}","key":"crn:${crn}","srcdb":"${srcdb}","matched":"crn:${crn}"}`
-
-  const response = await fetch(url, { method: 'POST', body })
-  const data = await response.text()
-  const json = JSON.parse(data)
-  const $ = cheerio.load(json.instructordetail_html)
-  return $('.instructor-detail').text()
-}
-
-const searchOsuClass = async classCode => {
-  if (!classCode) return
-
-  const url = 'https://classes.oregonstate.edu/api/?page=fose&route=search'
-  const body = `{"other":{"srcdb":"999999"},"criteria":[{"field":"alias","value":"${classCode}"}]}`
-
-  const response = await fetch(url, {
-    method: 'POST',
-    body,
-  })
-  const json = await response.json()
-  let result
-  if (json && json.results) {
-    if (json.results.length === 0) {
-      // todo: no result ?
-    }
-    if (json.results.length === 1) {
-      result = json.results[0]
-    } else {
-      // todo: multiple classes ?
-    }
-  } else {
-    // todo: error
+const fetchProf = async (url) => {
+  if (!url) {
+    throw Error('Failed to get an professor url')
   }
 
-  return result
+  const response = await fetch(url)
+  const body = await response.text()
+
+  const $ = cheerio.load(body)
+  const html = $('div[class="breakdown-wrapper"]')
+  const quality = html.find('.quality-header').find('.grade').text().trim()
+  const takeAgain = html.find('.takeAgain').find('.grade').text().trim()
+  const difficulty = html.find('.difficulty').find('.grade').text().trim()
+
+  return {
+    quality,
+    takeAgain,
+    difficulty,
+  }
+}
+
+let message = {
+  text: 'Enter class code',
+  attachments: [(text = 'Sub text...')],
 }
 
 const generateMessage = async (classCode) => {
   const osuClass = await searchOsuClass(classCode)
-  if (!osuClass) {
-    throw new Error(`No class found (${classCode})`)
-  }
-
-  const instructor = await getInstructor(osuClass.code, osuClass.crn, osuClass.srcdb)
-  if (!instructor) {
-    throw new Error(`No instructor found with (code:${osuClass.code}, crn:${osuClass.crn}, srcdb:${osuClass.srcdb})`)
-  }
-
+  const { code, crn, srcdb } = validateOsuClass(osuClass)
+  const instructor = await getInstructor(code, crn, srcdb)
   const profUrl = await searchProf(instructor)
-  if (!profUrl) {
-    throw new Error(`No url found with (${instructor})`)
-  }
-
-  const res = await fetchProf(profUrl)
-  if (!res) {
-    throw new Error(`No professor data from (${profUrl})`)
-  }
-
-  return res
+  const { quality, takeAgain, difficulty } =  await fetchProf(profUrl)
+  message.text = `quality: ${quality}, takeAgain: ${takeAgain}, difficulty: ${difficulty}`
 }
 
-let message = {
-  text: 'Text message',
-  attachments: [(text = 'Sub text...')],
-}
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
 
-  generateMessage(req.body.text)
-    .then(({ quality, takeAgain, difficulty }) => {
-      message.text = `quality: ${quality}, takeAgain: ${takeAgain}, difficulty: ${difficulty}`
-      console.log(message.text)
-    })
-    .catch((err) => {
-      console.log(err.toString())
-      message.text = err.toString()
-      console.log(message.text)
-    })
-    .finally(() => {
-      console.log(message.text)
-      const stringified = JSON.stringify(message)
-      res.end(stringified)
-    })
+  try {
+    if (req.body.text) {
+        await generateMessage(req.body.text)
+    }
+  } catch (err) {
+    message.text = err.toString()
+  } finally {
+    const stringified = JSON.stringify(message)
+    res.end(stringified)
+  }
 }
