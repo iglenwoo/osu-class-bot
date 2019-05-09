7c09e8798ae9d91a713e0adcf003f44f3212d0fb
Ingyu Woo <iglenwoo@gmail.com>
2019-05-09 09:42:27 -0700

    set port for production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
 
-const port = 4000
+const port = process.env.PORT || 4000;
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Class Beaver listening on port ${port}!`))
